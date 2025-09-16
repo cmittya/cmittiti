ad0caeb9bf59c283ddd0183261df1b90990dfef4
blyadskiynoname@gmail.com <bjornvgsskole@gmail.com>
2025-09-16 10:45:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Import Express.js module
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
+// Initialize the Express application
+const app = express();
+// Define the port number for the server to listen on
+const port = parseInt(process.env.PORT) || 8080;
+// Define a route for the root URL ('/') and specify the response
+app.get('/', (req, res) => {
+  res.send("Hei på deg!"); // Send "Hello, World!" as the response
+});
+// Send HTML File to web browser for the URL ('/index') 
+app.get('/1',function(req,res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+// Start the server and have it listen on the defined port
+app.listen(port, () => {
+  console.log("Server is running on http://localhost:${port}");
+}); //Log a message to the console indicating the server is running});
